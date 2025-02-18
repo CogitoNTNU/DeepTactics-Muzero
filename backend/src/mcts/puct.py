d1a71454c40084a79d85074aabe9d091776b38e5
Ludvig Øvrevik <ludvigho@ntnu.no>
2025-02-18 19:55:09 +0100

    feat: add PUCT score calculation for MCTS
    Co-authored-by: Eiriksol <Eiriksol@users.noreply.github.com>

---
+++ backend/src/mcts/puct.py
@@ -0,0 +1,7 @@
+﻿from numpy import argmax
+from src.config import Config
+from src.mcts.node import Node
+from math import sqrt, log
+
+def puct_score(config: Config, node: Node):
+    return argmax(node.value/node.visits + node.policy_value * sqrt((node.parent.visits)/(1+node.visits))*(config.c1 + log((node.parent.visits + config.c2 + 1) / config.c2)))
