5c28b1be9004a8d4f63dc8c12a7818d61e5df20a
Kristian Carlenius <kristian.carlenius@hotmail.com>
2025-02-18 18:57:50 +0100

    legger til fra muzero

--- backend/src/gameenv.py
+++ backend/src/gameenv.py
@@ -3,24 +3,16 @@ import ale_py
 
 class Environment(object):
     """The environment MuZero is interacting with."""
-    def step(self, action):
-        pass
-
-
-class game:
     def __init__(self, gamefile: str = 'ALE/Breakout-v5'):
         self.env = gym.make(gamefile, continuous = True)  # remove render_mode in training
         self.obs, self.info = self.env.reset()
         self.episode_over: bool = False
 
-    def register_action(self, action):
+    def step(self, action):
         action = self.env.action_space.sample() #remove line, now its random
         self.obs, self.reward, terminated, truncated, info = self.env.step(action)
-        self.episode_over = terminated or truncated
-    
-    def action_loop(self, networkdecider):
-        while not self.episode_over:
-            self.register_action(networkdecider)
+
+    def close(self):
         self.env.close()
 
 class Game(object):
