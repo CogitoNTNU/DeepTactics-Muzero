c3cf172ecf834cb6360898b7d861ea2ed72a0092
Ludvig Øvrevik <ludvigho@ntnu.no>
2025-02-20 18:43:37 +0100

    feat: implement MCTS run function for Monte Carlo tree search

---
+++ backend/src/mcts/run_mcts.py
@@ -0,0 +1,34 @@
+﻿from src.config import Config
+from src.mcts.node import Node
+
+
+def run_mcts(config: Config, 
+             root: Node, 
+             action_history: ActionHistory,
+             network: Network):
+    
+    min_max_stats = MinMaxStats(config.known_bounds)
+
+    for _ in range(config.num_simulations):
+        
+        history = action_history.clone()
+        node = root
+        search_path = [node]
+
+        while node.expanded():
+            action, node = select_child(config, node, min_max_stats)
+            history.add_action(action)
+            search_path.append(node)
+
+        # Inside the search tree we use the dynamics function to obtain the next
+        # hidden state given an action and the previous hidden state.
+        parent = search_path[-2]
+        network_output = network.recurrent_inference(parent.hidden_state,
+                                                     history.last_action())
+        expand_node(node, history.to_play(), history.action_space(), network_output)
+
+        backpropagate(search_path, 
+                      network_output.value, 
+                      history.to_play(),
+                      config.discount, 
+                      min_max_stats)
