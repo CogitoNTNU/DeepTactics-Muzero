0ab6913b8fe066a949b914efdffe371da793a442
Ludvig Øvrevik <ludvigho@ntnu.no>
2025-02-20 19:33:32 +0100

    feat: add select_child function for child node selection in PUCT algorithm

--- backend/src/mcts/puct.py
+++ backend/src/mcts/puct.py
@@ -3,5 +3,13 @@ from src.config import Config
 from src.mcts.node import Node
 from math import sqrt, log
 
+
+def select_child(config: Config,  node: Node):
+    
+    _, action, child = max((puct_score(config, node), action, child) for action, child in node.children.items())
+    return action, child
+    
+    # The score for a node is based on its value, plus an exploration bonus based on the prior.
+
 def puct_score(config: Config, node: Node):
     return argmax(node.value/node.visits + node.policy_value * sqrt((node.parent.visits)/(1+node.visits))*(config.c1 + log((node.parent.visits + config.c2 + 1) / config.c2)))
