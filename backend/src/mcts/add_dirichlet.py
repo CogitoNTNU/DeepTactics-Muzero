38375d7122b62e84e527d2401592b98e82811f19
Ludvig Øvrevik <ludvigho@ntnu.no>
2025-02-20 18:43:26 +0100

    feat: add exploration noise to MCTS using Dirichlet distribution

---
+++ backend/src/mcts/add_dirichlet.py
@@ -0,0 +1,11 @@
+﻿from src.config import Config
+from src.mcts.node import Node
+import numpy as np
+
+def add_exploration_noise(config: Config, node: Node):
+
+    actions = list(node.children.keys())
+    noise = np.random.dirichlet([config.root_dirichlet_alpha] * len(actions))
+    frac = config.root_exploration_fraction
+    for a, n in zip(actions, noise):
+        node.children[a].prior = node.children[a].prior * (1 - frac) + n * frac
