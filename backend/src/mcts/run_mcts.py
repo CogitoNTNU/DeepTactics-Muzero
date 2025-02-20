8d96f46936dc6711832c756dc21aa73801a0578e
Ludvig Øvrevik <ludvigho@ntnu.no>
2025-02-20 19:34:46 +0100

    feat: import necessary functions and classes for MCTS execution

--- backend/src/mcts/run_mcts.py
+++ backend/src/mcts/run_mcts.py
@@ -1,5 +1,10 @@
-﻿from src.config import Config
+﻿from backend.src.mcts import backpropagate
+from backend.src.mcts.expand import expand_node
+from backend.src.mcts.puct import select_child
+from backend.src.utils.minmaxstats import MinMaxStats
+from src.config import Config
 from src.mcts.node import Node
+from src.game.action_history import ActionHistory
 
 
 def run_mcts(config: Config, 
