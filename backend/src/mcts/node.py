9f81d45e1d4091679e6d37f41c67458118876f7b
Ludvig Øvrevik <ludvigho@ntnu.no>
2025-02-25 18:51:59 +0100

    feat: enhance Node class with additional attributes and methods for state management

--- backend/src/mcts/node.py
+++ backend/src/mcts/node.py
@@ -1,6 +1,5 @@
 ﻿from typing import Dict, Optional
 
-
 class Node:
     def __init__(self, parent: Optional['Node'], state: None, policy_value: float) -> None:
         self.children: Dict[int, Node] = {}
@@ -9,3 +8,15 @@ class Node:
         self.value = 0
         self.state = state
         self.policy_value = policy_value
+        self.to_play = None # player
+        self.hidden_state = None #the hidden state of the representation network
+        self.reward = 0
+        
+    def expanded(self) -> bool:
+        return len(self.children) > 0
+    
+    def value(self) -> float:
+        if self.visits == 0:
+            return 0
+        return self.value / self.visits
+    
