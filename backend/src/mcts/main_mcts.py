5c4625cece3a34f1bb49b1f31d6122a9cd1d3e82
Ludvig Øvrevik <ludvigho@ntnu.no>
2025-02-25 18:53:01 +0100

    feat: add main_mcts module for MCTS execution and update run_mcts for tree search iterations

---
+++ backend/src/mcts/main_mcts.py
@@ -0,0 +1,18 @@
+﻿from src.game.player import Player
+from src.game.action import Action
+from src.mcts.play_game import play_game
+from src.mcts.node import Node
+from src.mcts.expand import expand_node
+from src.mcts.puct import select_child
+from src.mcts.backpropagate import backpropagate
+from src.utils.minmaxstats import MinMaxStats
+from src.config import Config
+from src.game.action_history import ActionHistory
+from backend.architecture.src.network import RepresentationNetwork, PredictionNetwork
+
+
+player = Player()
+config = Config()
+network = RepresentationNetwork()
+
+play_game(config, network)
