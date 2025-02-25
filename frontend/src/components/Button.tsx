7667cf9c00dba009ebf3efe5075bf5f9aa84c29b
Christian Fredrik Johnsen <christian@johnsen.no>
2025-02-25 19:53:29 +0100

    feat: convert template vite typescript to use tailwind css
    This isn't actually useful, just a learning experience for Christian
    Fredrik :O
    The template file will be removed, and some MuZero-related setup will
    appear. ---> INCOMING <---

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,16 @@
+const Button = () => {
+    return (
+      <a
+        href="https://example.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition"
+      >
+        Click me
+      </a>
+    );
+  };
+
+  
+  export default Button;
+  
