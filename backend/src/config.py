4b5c66ccc4dd722abdb1bd93ddad97f961a7cfe2
Eirik Solberg <eirsolb@stud.ntnu.no>
2025-02-27 17:43:40 +0100

    chore: update config file with correct action_space

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,53 +1,57 @@
 from typing import Optional
 from src.utils.minmaxstats import KnownBounds
 
+
 def visit_softmax_temperature(num_moves, training_steps):
-            if training_steps < 100:
-                return 3
-            elif training_steps < 125:
-                return 2      
-            elif training_steps < 150:
-                return 1      
-            elif training_steps < 175:
-                return 0.5      
-            elif training_steps < 200:
-                return 0.250      
-            elif training_steps < 225:
-                return 0.125      
-            elif training_steps < 250:
-                return 0.075
-            else:
-                return 0.001
+    if training_steps < 100:
+        return 3
+    elif training_steps < 125:
+        return 2
+    elif training_steps < 150:
+        return 1
+    elif training_steps < 175:
+        return 0.5
+    elif training_steps < 200:
+        return 0.250
+    elif training_steps < 225:
+        return 0.125
+    elif training_steps < 250:
+        return 0.075
+    else:
+        return 0.001
+
 
 class Config:
     def __init__(
         self,
         visit_softmax_temperature_fn=visit_softmax_temperature,
         known_bounds: Optional[KnownBounds] = None,
-        action_space: int = 18, # 18 legal actions in atari
-        input_planes: int = 128, # 3 rbg planes * 32 last states + 32 last actions
-        height: int = 96, # Pixel height and with
+        action_space: int = 1,  # 1 legal actions in cartpole
+        input_planes: int = 128,  # 3 rbg planes * 32 last states + 32 last actions
+        height: int = 96,  # Pixel height and with
         width: int = 96,
-        num_input_moves: int = 32, # Number of moves that is used as input to representation model
-        max_moves: float = 50_000, # Max moves before game ends
+        # Number of moves that is used as input to representation model
+        num_input_moves: int = 32,
+        max_moves: float = 50_000,  # Max moves before game ends
         game_name: str = "ALE/Breakout-v5",
-        num_selfplay_games = 1_000_000,
-        max_replay_games = 125_000, # Replay buffer size
-        n_tree_searches = 50,
+        num_selfplay_games=1_000_000,
+        max_replay_games=125_000,  # Replay buffer size
+        n_tree_searches=50,
         epsilon: float = 0.001,
         discount: float = 0.997,
         c1: float = 1.25,
         c2: float = 19652,
-        diriclet_noise = 0.25,
-        dirichlet_exploaration_factor = 0.25, # Set this to 0 for deterministic prior probabilites
-        batch_size = 2048,
+        diriclet_noise=0.25,
+        # Set this to 0 for deterministic prior probabilites
+        dirichlet_exploaration_factor=0.25,
+        batch_size=2048,
         # encode_game_state_fn = encode_state_atari,
         # softmax_policy_fn = softmax_policy_atari_train,
-        info_print_rate = 10,
-        training_interval = 1_000,
-        num_training_rolluts = 5,
-        model_load_filename = "test",
-        model_save_filename = "test",
+        info_print_rate=10,
+        training_interval=1_000,
+        num_training_rolluts=5,
+        model_load_filename="test",
+        model_save_filename="test",
     ):
         # Only to keep the type checker happy
         # gym.register_envs(ale_py)
@@ -67,7 +71,8 @@ class Config:
         self.n_tree_searches = n_tree_searches
 
         self.dirichlet_noise_alpha = diriclet_noise
-        self.dirichlet_exploration_factor = dirichlet_exploaration_factor # e = 0.25 as seen in Alphago Zero paper
+        # e = 0.25 as seen in Alphago Zero paper
+        self.dirichlet_exploration_factor = dirichlet_exploaration_factor
         self.visit_softmax_temperature_fn = visit_softmax_temperature_fn
 
         # Training
@@ -79,7 +84,7 @@ class Config:
         self.model_save_filepath = "models/" + game_name + "/" + model_save_filename
 
         self.num_training_rolluts = num_training_rolluts
-    
+
         # PUCT parameters
         self.c1 = c1
         self.c2 = c2
@@ -92,10 +97,8 @@ class Config:
 
         # Logging
         self.info_print_rate = info_print_rate
-        
+
         self.known_bounds = known_bounds
-        
-        
-        
+
     # def init_game(self) -> gym.Env:
     #     pass
--- backend/src/mcts/play_game.py
+++ backend/src/mcts/play_game.py
@@ -7,26 +7,30 @@ from src.mcts.run_mcts import run_mcts
 from src.mcts.select_action import select_action
 from src.gameenv import Game
 
+
 def play_game(config: Config, network: Network):
-    
-    game = Game()
-    
-    
+
+    game = Game(config.action_space, config.discount)
+
     # game.history should be a list of actions taken.
     while not game.terminal() and len(game.action_history().history) < config.max_moves:
-        
+
         root = Node()
-        current_observation = game.make_image(-1) # should get the observation (state) from the env.
-        expand_node(root, game.to_play(), game.legal_actions(), network.initial_inference(current_observation)) # inital_inference should give the inital policy, value and hiddenstate (from representation network)
+        # should get the observation (state) from the env.
+        current_observation = game.make_image(-1)
+        # inital_inference should give the inital policy, value and hiddenstate (from representation network)
+        expand_node(root, game.to_play(), game.legal_actions(),
+                    network.initial_inference(current_observation))
 
         add_exploration_noise(config, root)
 
         # We then run a Monte Carlo Tree Search using only action sequences and the
         # model learned by the network.
         run_mcts(config, root, game.action_history(), network)
-        action = select_action(config, len(game.action_history().history), root, network) # select action, but with respect to the temperature
+        # select action, but with respect to the temperature
+        action = select_action(config, len(
+            game.action_history().history), root, network)
         game.apply(action)
         game.store_search_statistics(root)
-    
+
     return game
-        
