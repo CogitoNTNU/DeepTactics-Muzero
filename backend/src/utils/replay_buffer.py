8b2b4fa0251915f14d1db7c2e50531b498823781
Vegardhgr <vegard.groder@gmail.com>
2025-02-27 18:38:18 +0100

    Replay buffer fixed

--- backend/src/utils/replay_buffer.py
+++ backend/src/utils/replay_buffer.py
@@ -1,23 +1,15 @@
 import random
+import numpy as np
 
 class ReplayBuffer:
     def __init__(self):
         self.buffer_size: int = 10000
-        self.buffer: list[dict] = []  #Stores game trajectories
+        self.buffer: list[list[dict]] = []  #Stores game trajectories
         
     def update_buffer(self, game: Game):
-        buffer_entry: dict[list, list, list, list, list] = {
-            "states": game.states,
-            "actions": game.actions,
-            "rewards": game.rewards,
-            "policies": game.policies,
-            "values": game.values 
-        }
-        
         if len(self.buffer) == self.buffer_size: #Buffer reached max cap
             self.buffer.pop(0)
-            self.buffer.append(buffer_entry)
-            
+        self.buffer.append(game)
        
     #Retrieves a random trajectory from the buffer 
     def get_game_trajectory(self) -> dict:
@@ -26,20 +18,17 @@ class ReplayBuffer:
         return buffer_entry
     
     #Returns the rest of the trajectory from a random postion in a game
-    def get_positions(self, buffer_entry: dict, n_last_states: int = -999) -> dict:
-        state = random.randint(0, len(buffer_entry)) #Selects a random state in the game
-        if n_last_states == -999: # Not set
-            return buffer_entry[state:] #Returns all subsequent states after the randomly selected state
-        else:
-            #This probably generates an index error
-            return {key: value[state-n_last_states:state+1] for key, value in buffer_entry.items()} # Returns the n last states 
-        
-        
+    def get_positions(self, buffer_entry: list, history_length: int = 0, nr_of_next_states: int = 5) -> tuple:
+        game_state = random.randint(0, len(buffer_entry)) #Selects a random state in the game
+       
+        # Returns a tuple. First element is state concatenated with the last n states. Second element is the next n states.
+        return (np.concatenate(buffer_entry[game_state-history_length:game_state+1], axis=-1)
+                , buffer_entry[game_state+1:game_state+nr_of_next_states+1])         
+       
     #Retrieves a game trajectory from the replay buffer and returns the last n states.
-    #If n not specified, random selection is done.
-    def retrieve_game(self, n_last_states: int = -999) -> dict:
+    def retrieve_game(self, n_last_states: int = 0, nr_of_next_states: int = 5) -> tuple:
         buffer_entry = self.get_game_trajectory()
-        return self.get_positions(buffer_entry, n_last_states) #Returns the last n states of a trajectory from the replay buffer
+        return self.get_positions(buffer_entry, n_last_states, nr_of_next_states) #Returns the last n states of a trajectory from the replay buffer
     
     
     
