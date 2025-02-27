155ae7c83ff2691e16a97a763d530c90311fb781
Vegardhgr <vegard.groder@gmail.com>
2025-02-27 19:34:16 +0100

    self_play.py created. Adds the games to the replay buffer.

---
+++ backend/src/utils/shared_storage.py
@@ -0,0 +1,14 @@
+from networks.network import Network 
+
+class SharedStorage(object):
+    def __init__(self):
+        self._networks = {}
+
+    def latest_network(self) -> Network:
+        if self._networks:
+            return self._networks[max(self._networks.keys())]
+        else:
+            return Network()
+
+    def save_network(self, step: int, network: Network):
+        self._networks[step] = network
