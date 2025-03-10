c4cca638f7857bb3ea5ab129f6141f08a4b05112
Kristian Carlenius <kristian.carlenius@hotmail.com>
2025-03-10 08:01:58 +0100

    liten fix

--- backend/src/self_play.py
+++ backend/src/self_play.py
@@ -4,8 +4,7 @@ from src.networks.network import Network
 import mcts.play_game as play_game
 from utils.shared_storage import SharedStorage
 
-def self_play(config: Config, storage: SharedStorage, 
-              replayBuffer: ReplayBuffer):
+def self_play(config: Config, storage: SharedStorage, replayBuffer: ReplayBuffer):
     nr_of_games_to_play = 10000
     for i in range(1, nr_of_games_to_play):
         network = storage.latest_network()
