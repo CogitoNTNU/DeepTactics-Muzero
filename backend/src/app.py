e30338d3bb5a2fee5b46df2585ceb7b64fc9ac12
Christian Fredrik Johnsen <christian@johnsen.no>
2025-03-18 17:06:17 +0100

    fix: remove CORS from backend, add TODO on CORS in `frontend/nginx.conf`
    The setup is as follows:
    
    1) All requests go to Docker Container port 80.
    2) The reverse proxy (nginx) forwards `/api/` requests to container port
       8080
    
    Thus, the backend is seeing that all of its requests are coming from
    localhost 80. To implement CORS, you will need to check the HTTP header
    at the reverse proxy level.
    
    TODO: At reverse proxy level, check that all incoming requests to `/api/` (and perhaps to frontend as well) are coming from the MuZero website (whatever that ends up being, it may be something like `https://www.cogito-ntnu.no/projects/muzero`. This is simply to prevent Grandma from being fooled into ...
    
    There shouldn't be much security risks when I think about it, especially
    with CORS, since we have no plan on doing login or anything like that.

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,19 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [ # Allow frontend to access the backend
-    "http://localhost:9135",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-# Websockets ?? Interesting
-
 @app.post("/predict")
 async def predict(game_type: str, game_state: Union[str, dict]) -> dict:
     return {"game_type": game_type, "game_state": game_state}
