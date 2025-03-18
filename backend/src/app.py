6c7150fd8fbcc4ad033061a6d2cadc0246a11e38
Christian Fredrik Johnsen <christian@johnsen.no>
2025-03-18 17:55:18 +0100

    feat: add simple `ping` endpoint to backend API
    Used for testing that the frontend is able to communicate with the
    backend.

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.post("/predict")
 async def predict(game_type: str, game_state: Union[str, dict]) -> dict:
     return {"game_type": game_type, "game_state": game_state}
+
+@app.get("/ping")
+async def ping():
+    """Simple ping endpoint to check if the backend is running. Used for debugging."""
+    return {"message": "Hello, world! Backend is working! An update is coming soon!"}
