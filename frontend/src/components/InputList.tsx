1da327f17d597de455dad4b5edcf1e2f1f9cefad
Eirik Solberg <eirsolb@stud.ntnu.no>
2025-03-22 13:09:22 +0100

    feat: add title to inputlist and add all muzero config parameters to config page

--- frontend/src/components/InputList.tsx
+++ frontend/src/components/InputList.tsx
@@ -7,12 +7,14 @@ interface Parameter {
 }
 
 interface EnvironmentParametersFormProps {
+  title: string;
   parameters: Parameter[];
 }
 
-export default function EnvironmentParametersForm({ parameters }: EnvironmentParametersFormProps) {
+export default function EnvironmentParametersForm({ parameters, title }: EnvironmentParametersFormProps) {
   return (
     <div className="space-y-4">
+      <h2 className="text-lg font-semibold pb-5 underline">{title}</h2>
       {parameters.map((param, index) => (
         <InputWithTooltip key={index} {...param} />
       ))}
--- frontend/src/pages/MuZeroConfig.tsx
+++ frontend/src/pages/MuZeroConfig.tsx
@@ -1,5 +1,5 @@
 import InputList from "../components/InputList"
-import { environmentParameters } from "../config/muzeroParameters";
+import { environmentParameters, selfPlaySettings, explorationSettings, trainingParameters, neuralNetworkSettings, puctParameters, loggingSettings } from "../config/muzeroParameters";
 
 
 export default function MuZeroConfig() {
@@ -7,8 +7,14 @@ export default function MuZeroConfig() {
     <div className="flex flex-col items-center justify-center py-20">
       <h1 className="text-4xl font-bold">Welcome to the config site!</h1>
       <p className="mt-4 text-lg">Finetune the training configs here!</p>
-      <div className="pt-10">
-        <InputList parameters={environmentParameters} />
+      <div className="pt-10 flex flex-row justify-between w-full">
+        <InputList title="Enviroment" parameters={environmentParameters} />
+        <InputList title="Self Play" parameters={selfPlaySettings} />
+        <InputList title="Exploration" parameters={explorationSettings} />
+        <InputList title="Training" parameters={trainingParameters} />
+        <InputList title="Neural Network" parameters={neuralNetworkSettings} />
+        <InputList title="Puct" parameters={puctParameters} />
+        <InputList title="Logging" parameters={loggingSettings} />
       </div>
     </div>
   );
