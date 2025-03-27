80138704ae928ca7b7241e371fe5c711b3155c3d
Vegardhgr <vegard.groder@gmail.com>
2025-03-27 13:37:32 +0100

    Replay buffer clean up, and replay buffer test updated

--- backend/src/utils/replay_buffer.py
+++ backend/src/utils/replay_buffer.py
@@ -32,16 +32,16 @@ class ReplayBuffer:
         self.buffer.append(game)
        
     #Retrieves a random trajectory from the buffer 
-    def get_game_trajectory(self) -> dict:
-        """
-        Retrieves a random game trajectory from the buffer.
+    # def get_game_trajectory(self) -> dict:
+    #     """
+    #     Retrieves a random game trajectory from the buffer.
 
-        Returns:
-            dict: A randomly selected game trajectory.
-        """
-        entry = random.randint(0, len(self.buffer)-1)
-        buffer_entry = self.buffer[entry]
-        return buffer_entry
+    #     Returns:
+    #         dict: A randomly selected game trajectory.
+    #     """
+    #     entry = random.randint(0, len(self.buffer)-1)
+    #     buffer_entry = self.buffer[entry]
+    #     return buffer_entry
     
     def sample_batch(self, num_unroll_steps: int, td_steps: int, action_space_size: int) -> list:
         """
@@ -88,54 +88,54 @@ class ReplayBuffer:
         return np.random.choice(range(len(game.rewards) - 1))
     
     #Returns the rest of the trajectory from a random postion in a game
-    def get_positions(self, buffer_entry: list, history_length: int = 0, nr_of_next_states: int = 5) -> tuple:
-        """
-        Retrieves concatenated historical states and the following states from a trajectory.
+    # def get_positions(self, buffer_entry: list, history_length: int = 0, nr_of_next_states: int = 5) -> tuple:
+    #     """
+    #     Retrieves concatenated historical states and the following states from a trajectory.
 
-        If the sampled position is less than the required history length, the initial state is
-        repeated as needed for padding.
+    #     If the sampled position is less than the required history length, the initial state is
+    #     repeated as needed for padding.
 
-        Args:
-            buffer_entry (list): A game trajectory (list of dictionaries with state information).
-            history_length (int, optional): Number of previous states to include. Defaults to 0.
-            nr_of_next_states (int, optional): Number of future states to retrieve. Defaults to 5.
+    #     Args:
+    #         buffer_entry (list): A game trajectory (list of dictionaries with state information).
+    #         history_length (int, optional): Number of previous states to include. Defaults to 0.
+    #         nr_of_next_states (int, optional): Number of future states to retrieve. Defaults to 5.
 
-        Returns:
-            tuple: A tuple containing:
-                - history_states (np.ndarray): Concatenated states representing history.
-                - next_states (list): The next states following the sampled position.
-        """
-        game_state = random.randint(0, len(buffer_entry)-1) #Selects a random state in the game
-        # history_states = []
-        if game_state+1 <= history_length:
-            # We need to pad with (history_length + 1) - (game_state + 1) states.
-            pad_count = history_length - game_state
-            pad = [buffer_entry[0]["state"]] * pad_count  # Copy the initial state as many times as needed
-            history_list = pad + [entry["state"] for entry in buffer_entry[:game_state + 1]]
-            history_states = np.concatenate(history_list, axis=-1)
-        else:
-            # If enough states exist, take the last (history_length + 1) states.
-            history_list = [entry["state"] for entry in buffer_entry[game_state - history_length: game_state+1]]
-            history_states = np.concatenate(history_list, axis=-1)
+    #     Returns:
+    #         tuple: A tuple containing:
+    #             - history_states (np.ndarray): Concatenated states representing history.
+    #             - next_states (list): The next states following the sampled position.
+    #     """
+    #     game_state = random.randint(0, len(buffer_entry)-1) #Selects a random state in the game
+    #     # history_states = []
+    #     if game_state+1 <= history_length:
+    #         # We need to pad with (history_length + 1) - (game_state + 1) states.
+    #         pad_count = history_length - game_state
+    #         pad = [buffer_entry[0]["state"]] * pad_count  # Copy the initial state as many times as needed
+    #         history_list = pad + [entry["state"] for entry in buffer_entry[:game_state + 1]]
+    #         history_states = np.concatenate(history_list, axis=-1)
+    #     else:
+    #         # If enough states exist, take the last (history_length + 1) states.
+    #         history_list = [entry["state"] for entry in buffer_entry[game_state - history_length: game_state+1]]
+    #         history_states = np.concatenate(history_list, axis=-1)
             
-        # Returns a tuple. First element is state concatenated with the last n states. Second element is the next n states.
-        next_state = game_state+1
+    #     # Returns a tuple. First element is state concatenated with the last n states. Second element is the next n states.
+    #     next_state = game_state+1
 
-        return (history_states, buffer_entry[next_state:next_state+nr_of_next_states])
+    #     return (history_states, buffer_entry[next_state:next_state+nr_of_next_states])
        
-    def retrieve_game(self, n_last_states: int = 0, nr_of_next_states: int = 5) -> tuple:
-        """
-        Retrieves a game trajectory from the buffer and returns its last n states and the next states.
+    # def retrieve_game(self, n_last_states: int = 0, nr_of_next_states: int = 5) -> tuple:
+    #     """
+    #     Retrieves a game trajectory from the buffer and returns its last n states and the next states.
 
-        Args:
-            n_last_states (int, optional): Number of last states to include in the history. Defaults to 0.
-            nr_of_next_states (int, optional): Number of subsequent states to retrieve. Defaults to 5.
+    #     Args:
+    #         n_last_states (int, optional): Number of last states to include in the history. Defaults to 0.
+    #         nr_of_next_states (int, optional): Number of subsequent states to retrieve. Defaults to 5.
 
-        Returns:
-            tuple: A tuple containing the concatenated history states and the subsequent states.
-        """
-        buffer_entry = self.get_game_trajectory()
-        return self.get_positions(buffer_entry, n_last_states, nr_of_next_states) #Returns the last n states of a trajectory from the replay buffer
+    #     Returns:
+    #         tuple: A tuple containing the concatenated history states and the subsequent states.
+    #     """
+    #     buffer_entry = self.get_game_trajectory()
+    #     return self.get_positions(buffer_entry, n_last_states, nr_of_next_states) #Returns the last n states of a trajectory from the replay buffer
     
     def last_game(self) -> Game:
         """
