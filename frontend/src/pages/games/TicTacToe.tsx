ecef63cd69671305ba8822bb9ef3f776506fc32e
Christian Fredrik Johnsen <christian@johnsen.no>
2025-04-01 18:58:01 +0200

    feat: add basic TicTacToe page
    Doesn't contain anything of use yet, will get some basic 3x3 grid up and
    running soon.

---
+++ frontend/src/pages/games/TicTacToe.tsx
@@ -0,0 +1,9 @@
+export default function TicTacToe() {
+    return (
+      <div className="flex flex-col items-center justify-center py-20">
+        <h1 className="text-4xl font-bold">Tic Tac Toe</h1>
+        <p className="mt-4 text-lg">Official page for Tic Tac Toe</p>
+      </div>
+    );
+  }
+ 
