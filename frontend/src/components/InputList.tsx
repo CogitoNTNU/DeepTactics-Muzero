3ce6c69342b606278d7314a6dc41ea13a4721ef0
Eirik Solberg <eirsolb@stud.ntnu.no>
2025-04-03 18:11:20 +0200

    style: add padding to config page

--- frontend/src/components/InputList.tsx
+++ frontend/src/components/InputList.tsx
@@ -36,7 +36,7 @@ export default function EnvironmentParametersForm({
   };
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 px-2">
       <h2 className="text-lg font-semibold pb-5 underline">{title}</h2>
 
       {parameters.map((param, index) => (
