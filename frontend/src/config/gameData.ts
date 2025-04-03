2b45a9328da18e4ac54e85400017c82028e80842
Eirik Solberg <eirsolb@stud.ntnu.no>
2025-04-03 19:33:45 +0200

    refactor: clean code v2

---
+++ frontend/src/config/gameData.ts
@@ -0,0 +1,20 @@
+export const games = [
+  {
+    title: 'Othello',
+    description: 'Othello er et strategisk brettspill for to spillere der målet er å omringe og snu motstanderens brikker for å dominere brettet.',
+    imageUrl: 'https://www.eothello.com/images/how_to_play_othello_0.png',
+    link: './othello',
+  },
+  {
+    title: 'Cartpole',
+    description: 'CartPole er en klassisk kontrollproblem der målet er å balansere en stang på en vogn ved å justere vogna\'s bevegelser.',
+    imageUrl: 'https://miro.medium.com/v2/resize:fit:1188/1*LVoKXR7aX7Y8npUSsYZdKg.png',
+    link: './cartpole',
+  },
+  {
+    title: 'TicTacToe',
+    description: 'Tic-Tac-Toe er et spill der to spillere bytter på å plassere X eller O på et 3x3 rutenett for å få tre på rad.',
+    imageUrl: 'https://cdn-icons-png.flaticon.com/512/10199/10199746.png',
+    link: './tictactoe',
+  },
+];
