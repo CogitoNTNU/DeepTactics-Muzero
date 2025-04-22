b4f444ced0c36b1011df2c8e11b5040e10e36f07
Christian Fredrik Johnsen <christian@johnsen.no>
2025-04-22 17:39:21 +0200

    chore: Add picture of Adi to `public/images/` and display in *About us* page.

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,13 +1,13 @@
 import ImageCard from "../components/ImageCard";
 
 const teamMembers = [
-  { imageSrc: "/images/placeholder.jpg", title: "Adi", description: "Robotikk", github: "https://github.com/adisinghstudent", linkedin: "https://www.linkedin.com/in/adisinghwork/" },
+  { imageSrc: "/images/adi.JPG", title: "Adi", description: "Robotikk", github: "https://github.com/adisinghstudent", linkedin: "https://www.linkedin.com/in/adisinghwork/" },
   { imageSrc: "/images/brage.JPG", title: "Brage", description: "Datateknologi", github: "https://github.com/BrageHK", linkedin: "https://www.linkedin.com/in/brage-kvamme-b33318212/" },
   { imageSrc: "/images/christian.JPG", title: "Christian", description: "Datateknologi", github: "https://github.com/ChristianFredrikJohnsen", linkedin: undefined },
   { imageSrc: "/images/eirik.JPG", title: "Eirik", description: "Datateknologi", github: "https://github.com/Eiriksol", linkedin: "https://no.linkedin.com/in/eirik-solberg-b25bb0252" },
   { imageSrc: "/images/kristian.JPG", title: "Kristian", description: "Datateknologi", github: "https://github.com/kristiancarlenius", linkedin: undefined },
   { imageSrc: "/images/ludvig.JPG", title: "Ludvig", description: "Informatikk", github: "https://github.com/ludvigovrevik", linkedin: "https://no.linkedin.com/in/ludvig-øvrevik-436831214" },
-  { imageSrc: "/images/magnus.JPG", title: "Magnus", description: "Dataingeniør", github: undefined, linkedin: "https://no.linkedin.com/in/magnus-wang-wold-bb6b16247" },
+  { imageSrc: "/images/magnus.JPG", title: "Magnus", description: "Dataingeniør", github: "https://github.com/Magnuww", linkedin: "https://no.linkedin.com/in/magnus-wang-wold-bb6b16247" },
   { imageSrc: "/images/nicolai.JPG", title: "Nicolai", description: "Datateknologi", github: "https://github.com/Nicolai9897", linkedin: undefined },
   { imageSrc: "/images/vegard.JPG", title: "Vegard", description: "Datateknologi", github: "https://github.com/Vegardhgr", linkedin: "https://www.linkedin.com/in/vegard-gr%C3%B8der-b04ab1261/" },
 ];
